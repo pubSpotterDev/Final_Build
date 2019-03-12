ba940f4364aa90d5fafe772bd86ea408531f04ee
Buzz-Alpha <44171515+Buzz-Alpha@users.noreply.github.com>
2019-03-12 21:32:21 +0000

    Update ControllerServer.java

--- src/Controller/ControllerServer.java
+++ src/Controller/ControllerServer.java
@@ -8,7 +8,7 @@ public class ControllerServer
 {
 	public static void main(String[] args) throws Exception
 	{
-		Server server = new Server(8010); //Set server to port 8005
+		Server server = new Server(8010); //Set server to port 8010
 		WebAppContext ctx = new WebAppContext();
 		ctx.setResourceBase("webapp"); //Using the webapp folder 
 		ctx.setContextPath("/pubspotter");
